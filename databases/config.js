6ae82ef26732503384f09a054d5959d19f799050
anthony <anthonysa0813@gmail.com>
2022-09-04 19:02:13 -0500

    resolve some bugs

--- databases/config.js
+++ databases/config.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     mongoose.connect(process.env.MONGO_CNN);
     console.log("Se conectó a la base de datos :D");
   } catch (error) {
-    console.log(error);
     throw new Error("Hubo un error en la conexión a la base de datos....");
   }
 };
